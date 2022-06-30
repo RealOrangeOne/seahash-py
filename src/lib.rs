6f99db1210379c8f7ab1468f890c78a7df27d2dc
Jake Howard <git@theorangeone.net>
2022-06-30 08:16:09 +0100

    Implement core seahash functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,21 @@
-
 use pyo3::prelude::*;
 
-/// Formats the sum of two numbers as string.
+/// Hash some bytes
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<usize> {
-    Ok(a + b)
+fn hash(buf: &[u8]) -> PyResult<u64> {
+    Ok(seahash::hash(buf))
+}
+
+/// Hash some bytes according to a chosen seed.
+#[pyfunction]
+fn hash_seeded(buf: &[u8], a: u64, b: u64, c: u64, d: u64) -> PyResult<u64> {
+    Ok(seahash::hash_seeded(buf, a, b, c, d))
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn seahash(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(hash, m)?)?;
+    m.add_function(wrap_pyfunction!(hash_seeded, m)?)?;
     Ok(())
 }
