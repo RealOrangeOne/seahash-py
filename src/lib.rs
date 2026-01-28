100889e604006edd13caa3eda556fdffdf7cf52e
Stephen Finucane <stephen@that.guru>
2026-01-28 14:21:30 +0000

    Bump pyo3 version
    Signed-off-by: Stephen Finucane <stephen@that.guru>

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 use pyo3::prelude::*;
+use pyo3::types::PyModule;
 
 mod inner {
     use std::hash::Hasher;
 
-    #[cfg(Py_3_11)]
     use pyo3::buffer::PyBuffer;
     use pyo3::prelude::*;
     use pyo3::types::PyBytes;
@@ -11,13 +11,13 @@ mod inner {
     /// Hash some bytes
     #[pyfunction]
     pub(crate) fn hash(py: Python, buf: &[u8]) -> u64 {
-        py.allow_threads(|| seahash::hash(buf))
+        py.detach(|| seahash::hash(buf))
     }
 
     /// Hash some bytes according to a chosen seed.
     #[pyfunction]
     pub(crate) fn hash_seeded(py: Python, buf: &[u8], a: u64, b: u64, c: u64, d: u64) -> u64 {
-        py.allow_threads(|| seahash::hash_seeded(buf, a, b, c, d))
+        py.detach(|| seahash::hash_seeded(buf, a, b, c, d))
     }
 
     #[derive(Copy, Clone)]
@@ -44,24 +44,18 @@ mod inner {
             SeaHash { inner }
         }
 
-        #[cfg(not(Py_3_11))]
-        pub fn update(&mut self, py: Python, obj: &[u8]) {
-            py.allow_threads(|| self.inner.write(obj))
-        }
-
-        #[cfg(Py_3_11)]
-        pub fn update(&mut self, py: Python, obj: &PyAny) -> PyResult<()> {
-            if let Ok(buf) = obj.extract() {
-                py.allow_threads(|| self.inner.write(buf));
+        pub fn update(&mut self, py: Python, obj: &Bound<'_, PyAny>) -> PyResult<()> {
+            if let Ok(buf) = obj.extract::<Vec<u8>>() {
+                py.detach(|| self.inner.write(&buf));
             } else {
-                let vec = PyBuffer::get(obj)?.to_vec(py)?;
-                py.allow_threads(|| self.inner.write(vec.as_slice()));
+                let vec: Vec<u8> = PyBuffer::get(obj)?.to_vec(py)?;
+                py.detach(|| self.inner.write(vec.as_slice()));
             }
             Ok(())
         }
 
-        pub fn digest(&self, py: Python) -> PyObject {
-            PyBytes::new(py, &self.intdigest().to_ne_bytes()).into()
+        pub fn digest<'py>(&self, py: Python<'py>) -> Bound<'py, PyBytes> {
+            PyBytes::new(py, &self.intdigest().to_ne_bytes())
         }
 
         pub fn intdigest(&self) -> u64 {
@@ -80,7 +74,7 @@ mod inner {
 }
 /// A Python module implemented in Rust.
 #[pymodule]
-fn seahash(_py: Python, m: &PyModule) -> PyResult<()> {
+fn seahash(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add("__version__", env!("CARGO_PKG_VERSION"))?;
     m.add_function(wrap_pyfunction!(inner::hash, m)?)?;
     m.add_function(wrap_pyfunction!(inner::hash_seeded, m)?)?;
